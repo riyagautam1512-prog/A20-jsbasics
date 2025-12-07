2cc98d9c0c634f1f24b5bbde98a12fd6816f0b35
riyagautam1512 <riyagautam1512@gmail.com>
2025-12-07 07:56:42 +0545

    feat: js basics

---
+++ 3rdday.js
@@ -0,0 +1,26 @@
+//const students = ["riya","anusha", "bipasha"];
+
+//const collegedetails = {
+    //name:"techspire",
+    //:"A20",
+    //:students,
+    //award:null,
+
+//};
+//console.log(collegedetails.students[2]);
+
+//getStudentName ();
+//function getStudentName() {
+  //  console.log("i am normal func");
+}
+
+getStudentsName();
+const getStudentName =() =>{
+    console.log("i am arrow func")
+
+};
+
+
+
+
+
